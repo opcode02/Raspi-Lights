073e712ba5cf61f53971f932ae0f0c307ca93a99
Shane Warren <shane.warren@gmail.com>
2014-11-27 15:43:03 -0600

    Added more colors
    Added more colors after I figured out why mixed colors stopped working
    near the end of my strands.  Had to run power/ground about half way up
    the strands and colors all look correct now.

--- test1.py
+++ test1.py
@@ -7,23 +7,28 @@ from LedStrip_WS2801 import *
 
 nLeds = 138
 
-Red = [255, 0, 0]
-Green = [0, 255, 0]
-Blue = [0, 0, 255]
+Red = [0xFF, 0, 0]
+Green = [0, 0xFF, 0]
+Blue = [0, 0, 0xFF]
+#Orange = [0xFF, 0x66, 0]
+Orange = [0xCC, 0x33, 0]
+#Violet = [0xCC, 0, 0xCC]
+Violet = [0x3F, 0, 0x3F] # For a darker purple linearly decrease red and blue
 
 # create ledstrip object
 l = LedStrip_WS2801(nLeds, 2)
 
-toggle = itertools.cycle([Red, Green, Blue]).next
+toggle = itertools.cycle([Red, Orange, Green, Blue, Violet]).next
 
 for pixel in range(0, nLeds):    
     color = toggle()
     l.setPixel(pixel, color)
-    l.update()
+
+l.update()
 
 while 1:
     l.copyBuffer(0, 1)
-    for n in range(0,99):
+    for n in range(0,100):
         l.copyBuffer(1, 0)
         l.rotate(n+1) 
         l.update()
